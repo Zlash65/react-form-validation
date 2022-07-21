61e80ac568111bd7de9660be065a0b1ff5ff8232
Zlash65 <zarrar65@gmail.com>
2022-07-21 19:39:24 -0400

    add signup page with form validations

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./FormInput.css";
+
+const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    const value = e.target.value;
+    if(value.length > 0) {
+      setFocused(true);
+    } else {
+      setFocused(false);
+    }
+  };
+
+  return (
+    <div className="formInput">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onInput={(e) =>
+          inputProps.name === "confirmPassword" &&
+          e.target.value.length > 0 && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default FormInput;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { positions, Provider } from 'react-alert';
+import AlertMUITemplate from 'react-alert-template-mui';
+
+const options = {
+  position: positions.MIDDLE
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <Provider template={AlertMUITemplate} {...options}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
